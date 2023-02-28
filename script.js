48b04fc4f252ddead86d14354f3586cac020e19c
unknown <leojoseph805@gmail.com>
2023-02-28 10:03:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const myImage = document.querySelector('img');
+
+fetch('flowers.jpg')
+.then(function(response) {
+  return response.blob();
+})
+.then(function(myBlob) {
+  const objectURL = URL.createObjectURL(myBlob);
+  myImage.src = objectURL;
+});
